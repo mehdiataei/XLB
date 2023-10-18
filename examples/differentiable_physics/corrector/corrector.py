fd0697ac798850f5c771742d2f27bbde39632c15
Mehdi Ataeei <mehdi.ataei@autodesk.com>
2023-10-18 15:19:56 -0400

    Converging

--- examples/differentiable_physics/corrector/corrector.py
+++ examples/differentiable_physics/corrector/corrector.py
@@ -30,6 +30,24 @@ class Cavity(BGKSim):
         vel_wall[:, 0] = prescribed_vel
         self.BCs.append(EquilibriumBC(tuple(moving_wall.T), self.gridInfo, self.precisionPolicy, rho_wall, vel_wall))
 
+# class UNet(nn.Module):
+#     @nn.compact
+#     def __call__(self, x):
+#         # Encoder
+#         x1 = nn.Conv(features=64, kernel_size=(3, 3), strides=(1, 1), padding="SAME")(x)
+#         x1 = nn.relu(x1)
+#         x2 = nn.Conv(features=128, kernel_size=(3, 3), strides=(2, 2), padding="SAME")(x1)
+#         x2 = nn.relu(x2)
+
+#         # Decoder
+#         x3 = nn.ConvTranspose(features=64, kernel_size=(3, 3), strides=(2, 2), padding="SAME")(x2)
+#         x3 = nn.relu(x3)
+#         x3 = nn.Conv(features=9, kernel_size=(3, 3), strides=(1, 1), padding="SAME")(x3)
+        
+#         x4 = nn.ConvTranspose(features=9, kernel_size=(3, 3), strides=(2, 2), padding="SAME")(x3)
+        
+#         return x4
+    
 class UNet(nn.Module):
     @nn.compact
     def __call__(self, x):
@@ -42,12 +60,13 @@ class UNet(nn.Module):
         # Decoder
         x3 = nn.ConvTranspose(features=64, kernel_size=(3, 3), strides=(2, 2), padding="SAME")(x2)
         x3 = nn.relu(x3)
-        x3 = nn.Conv(features=9, kernel_size=(3, 3), strides=(1, 1), padding="SAME")(x3)
         
-        x4 = nn.ConvTranspose(features=9, kernel_size=(3, 3), strides=(2, 2), padding="SAME")(x3)
+        x4 = nn.Conv(features=64, kernel_size=(3, 3), strides=(1, 1), padding="SAME")(x3)
+        x4 = nn.relu(x4)
         
-        return x4
-
+        x_out = nn.Conv(features=9, kernel_size=(3, 3), strides=(1, 1), padding="SAME")(x4)
+        
+        return x_out
 
 if __name__ == "__main__":
     precision = "f32/f32"
@@ -59,7 +78,7 @@ if __name__ == "__main__":
     nx_high = 128
     ny_high = 128
 
-    Re = 200.0
+    Re = 100.0
     prescribed_vel = 0.05
     clength_low = nx_low - 1
     clength_high = nx_high - 1
@@ -94,7 +113,6 @@ if __name__ == "__main__":
     sim_high = Cavity(**kwargs_high)
 
     input_shape = (nx_low, ny_low, 9)
-    f_low = sim_low.assign_fields_sharded()
     f_high = sim_high.assign_fields_sharded()
 
     model = UNet()
@@ -103,25 +121,37 @@ if __name__ == "__main__":
     optimizer_state = optimizer.init(params)
 
     model = UNet()
-
     def scan_fn(state, _):
         f_low, timestep = state
         f_low, _ = sim_low.step(f_low, timestep)
-        return (corrected_f_low, timestep + 1), None
+        return (f_low, timestep + 1), None
 
-    def update_step(num_timesteps, f_high):
-        state, _ = lax.scan(scan_fn, initial_state, jnp.arange(num_timesteps))
-        f_low = state[0]
-        corrected_f_low = model.apply(params, f_low)            
-        loss = jnp.mean((corrected_f_low - f_high)**2)
-        return loss, corrected_f_low
+    max_timestep = 10000
+    f_high = sim_high.run(max_timestep)
+    f_high_downsampled = downsample_field(f_high, 2)
 
-    num_timesteps = 100
-    for i in range(num_timesteps):
-        f_high, _ = sim_high.step(f_high, i)
-    initial_state = (f_low, 0)
-    grads = jax.grad(update_step, static_argnums=(1,))(num_timesteps, f_high)
+    def loss_fn(params, f_high_downsampled):
+        max_timestep = 10000
+        application_steps = 100
+        f_low = sim_low.assign_fields_sharded()
+        for i in range(max_timestep // application_steps):
+            print("Application step: ", i)
+            print(f"Iteration: {i * application_steps}")
+            f_low, _ = lax.scan(scan_fn, (f_low, application_steps * i), jnp.arange(application_steps))
+            f_low = f_low[0]
+            f_low += model.apply(params, f_low)
+        return jnp.mean((f_low - f_high_downsampled) ** 2)
 
-    # Update autoencoder parameters
-    updates, optimizer_state = optimizer.update(grads, optimizer_state)
-    new_params = optax.apply_updates(params, updates)
+    def update(params, optimizer_state, f_high_downsampled):
+        loss, grad = jax.value_and_grad(loss_fn)(params, f_high_downsampled)
+        updates, optimizer_state = optimizer.update(grad, optimizer_state)
+        new_params = optax.apply_updates(params, updates)
+        return new_params, optimizer_state, loss
+
+    epochs = 10  # Number of training epochs
+    for epoch in range(epochs):
+        timestep = 0 
+        # Calculate loss and update parameters
+        params, optimizer_state, loss = update(params, optimizer_state, f_high_downsampled)
+
+        print(f"Epoch {epoch + 1}, Loss: {loss}")
